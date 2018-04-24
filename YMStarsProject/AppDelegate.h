7a78c625255219fc0bf6d55c2cc8f8c248e0cdf3
huangcanhui <HCH@CHHuangdeiMac.local>
2018-04-24 13:42:17 +0800

    Initial Commit

---
+++ YMStarsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YMStarsProject
+//
+//  Created by huangcanhui on 2018/4/24.
+//  Copyright © 2018年 黄灿辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
